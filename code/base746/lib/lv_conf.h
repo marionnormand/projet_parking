b403822e2fb59c08c28b24d92c1d8f01dc084642
marion normand <marion.normand.80@gmail.com>
2026-05-27 20:32:27 +0200

    debut de code

---
+++ code/base746/lib/lv_conf.h
@@ -0,0 +1,9 @@
+/* Forwarding header so lv_conf_internal.h finds lv_conf.h
+   Expected location: ../lib/lv_conf.h relative to lvgl/src
+*/
+#ifndef LV_CONF_FORWARD_H
+#define LV_CONF_FORWARD_H
+
+#include "lvgl/lv_conf.h"
+
+#endif /* LV_CONF_FORWARD_H */
---
+++ code/base746/src/main.cpp
@@ -0,0 +1,129 @@
+#include "../lib/lvgl/lvgl.h"
+
+
+static void event_handler(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+
+    if(code == LV_EVENT_CLICKED) {
+        LV_LOG_USER("Clicked");
+    }
+    else if(code == LV_EVENT_VALUE_CHANGED) {
+        LV_LOG_USER("Toggled");
+    }
+}
+
+void testLvgl()
+{
+  // Initialisations générales
+  lv_obj_t * label;
+
+  lv_obj_t * btn1 = lv_button_create(lv_screen_active());
+  lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
+  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
+  lv_obj_remove_flag(btn1, LV_OBJ_FLAG_PRESS_LOCK);
+
+  label = lv_label_create(btn1);
+  lv_label_set_text(label, "Button");
+  lv_obj_center(label);
+
+  lv_obj_t * btn2 = lv_button_create(lv_screen_active());
+  lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_ALL, NULL);
+  lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
+  lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
+  lv_obj_set_height(btn2, LV_SIZE_CONTENT);
+
+  label = lv_label_create(btn2);
+  lv_label_set_text(label, "Toggle");
+  lv_obj_center(label);
+}
+
+#ifdef ARDUINO
+
+#include "lvglDrivers.h"
+
+
+#define PA0 PIN_A0
+
+const int PIN_IR = PA0;       // Broche analogique
+const int SEUIL  = 500;      // Seuil de détection (0–1023)
+
+// à décommenter pour tester la démo
+#include "demos/lv_demos.h"
+#include "lvgl.h"
+#include <demos/widgets/lv_demo_widgets.h>
+
+
+int recup_infrarouge() {
+  int val = analogRead(PIN_IR);
+  lv_obj_t * label = lv_label_create(lv_scr_act());
+  if (val > SEUIL) {
+    lv_label_set_text(label, "Objet détecté !");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    return 1;
+  }
+  else {
+    lv_label_set_text(label, "Aucun objet détecté");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    return 0;
+  }
+}
+
+void init_system() {
+  pinMode(PIN_IR, INPUT);
+}
+
+void mySetup()
+{
+  // à décommenter pour tester la démo
+  // lv_demo_widgets();
+
+  // Initialisations générales
+  init_system();
+  // testLvgl();
+
+}
+
+void loop()
+{
+  // Inactif (pour mise en veille du processeur)
+  recup_infrarouge();
+}
+
+void myTask(void *pvParameters)
+{
+  // Init
+  TickType_t xLastWakeTime;
+  // Lecture du nombre de ticks quand la tâche débute
+  xLastWakeTime = xTaskGetTickCount();
+  while (1)
+  {
+    // Loop
+
+    // Endort la tâche pendant le temps restant par rapport au réveil,
+    // ici 200ms, donc la tâche s'effectue toutes les 200ms
+    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(200)); // toutes les 200 ms
+  }
+}
+
+#else
+
+#include "lvgl.h"
+#include "app_hal.h"
+#include <cstdio>
+
+int main(void)
+{
+  printf("LVGL Simulator\n");
+  fflush(stdout);
+
+  lv_init();
+  hal_setup();
+
+  testLvgl();
+
+  hal_loop();
+  return 0;
+}
+
+#endif
